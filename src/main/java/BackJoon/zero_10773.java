f09afc14e681ecabde66f47b235c253f1a1c2667
Juwon-Yun <eooll2@daum.net>
2022-01-19 15:28:24 +0900

    10773

---
+++ src/main/java/BackJoon/zero_10773.java
@@ -0,0 +1,28 @@
+package BackJoon;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class zero_10773 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder builder = new StringBuilder();
+		int max_idx = sc.nextInt();
+		int sum = 0;
+
+		for(int i =0; i < max_idx; i++){
+			int num = sc.nextInt();
+			if(num == 0){
+				stack.pop();
+			}else{
+				stack.push(num);
+			}
+		}
+
+		for (Integer element : stack) {
+			sum += element;
+		}
+		System.out.println(sum);
+	}
+}
