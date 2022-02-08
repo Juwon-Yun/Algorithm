4dacb5f999f43986acb6ba6b7313ffb5bd474148
윤주원 <eooll2@daum.net>
2022-02-08 17:18:21 +0900

    괄호_9012

---
+++ src/main/java/BackJoon/괄호_9012.java
@@ -0,0 +1,14 @@
+package BackJoon;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 괄호_9012 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+    }
+}
