815f7af9607eed7ecaa9dc19ea84ddc0aa5a7d87
Juwon-Yun <eooll2@daum.net>
2022-02-12 15:10:31 +0900

    피보나치2

---
+++ src/main/java/programmers/피보나치수_2.java
@@ -0,0 +1,32 @@
+package programmers;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 피보나치수_2 {
+
+	static  long[] arr;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		arr = new long[N  + 1];
+
+		for(int i = 0; i < N + 1; i++){
+			arr[i] = -1;
+		}
+
+		arr[0] = 0;
+		arr[1] = 1;
+
+		System.out.println(fibo(N));
+	}
+	public static long fibo(int n){
+		if(arr[n] == -1){
+			arr[n] = fibo(n - 1) + fibo(n - 2);
+		}
+		return arr[n];
+	}
+}
